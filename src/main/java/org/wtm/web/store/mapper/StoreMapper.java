1c5a23b37323512c62d3f85e856e9aa9ee02bb9c
rak222 <rak2042@daum.net>
2024-11-19 13:01:54 +0900

    기능 개선 진행중

--- src/main/java/org/wtm/web/store/mapper/StoreMapper.java
+++ src/main/java/org/wtm/web/store/mapper/StoreMapper.java
@@ -15,13 +15,19 @@ public class StoreMapper {
         // 소수 첫째 자리까지 반올림
         rating = Math.round(rating * 10) / 10.0;
 
+        // 하드코딩된 userId
+        Long userId = 1L;
+
+        boolean isBookmarked = store.getBookmarks().stream()
+                .anyMatch(bookmark -> bookmark.getUser().getId().equals(userId));
+
         return StoreResponseDto.builder()
                 .storeId(store.getId())
                 .name(store.getName())
                 .address(store.getAddress())
                 .operatingHours(store.getOpenTime() + " - " + store.getCloseTime())
                 .price(store.getTickets().isEmpty() ? null : store.getTickets().get(0).getPrice())
-                .isBookmarked(!store.getBookmarks().isEmpty())
+                .isBookmarked(isBookmarked)
                 .img(store.getUser().getProfilePicture())
                 .rating(rating)
                 .build();
